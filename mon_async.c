f8ca63f35c3a871db9744650503c05abf6b8b3eb
Peter Downs <peter.l.downs@gmail.com>
2011-06-24 13:28:02 -0400

    monitor now connects to hostname/port

--- mon_async.c
+++ mon_async.c
@@ -6,9 +6,32 @@
 #include <sys/socket.h>
 
 #include <event2/event.h>
+#include <event2/dns.h>
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
 #include <event2/util.h>
+
+#define EB_HOST "localhost"
+#define EB_PORT 8080
+#define NUM_CONS 1
+
+typedef struct data_con {
+	struct bufferevent *bev;
+	char name[50];
+	char host[50];
+	int port;
+} data_con;
+
+void setup_con(data_con *con, char* nm, char *hst, int prt){
+	memset(&(con->name), '\0', sizeof con->name);
+	memset(&(con->host), '\0', sizeof con->host);
+	strncpy(con->name, nm, strlen(nm));
+	strncpy(con->host, hst, strlen(hst));
+	con->port = prt;
+	printf("con->name=%s\n", con->name);
+	printf("con->host=%s\n", con->host);
+	printf("con->port=%d\n", con->port);
+}
 
 void eventcb(struct bufferevent *bev, short events, void *ptr)
 {
@@ -47,36 +70,74 @@ void signalcb(evutil_socket_t sig, short events, void *user_data){
 int main(int argc, char* argv[])
 {
 	struct event_base *base;
-	struct bufferevent *bev;
+	struct evdns_base *dns_base;
+	
+	// Array of possible monitoring connections
+	printf("creating arry of cons\n");
+	data_con cons[NUM_CONS];
+	printf("setting up first con\n");
+	setup_con(&(cons[0]), "Event Builder", "localhost", 8080);
+	printf("finished setting up first con\n");
+
+	//struct bufferevent *bev;
 	struct event *signal_event;
+	
 	struct sockaddr_in sin;
 
+	// Create the bases
 	base = event_base_new();
+	dns_base = evdns_base_new(base, 1);
+	printf("created bases");
 
-	memset(&sin, 0, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(8080); /* Port 8080 */
+	// Create our sockets
+	int i;
+	for(i = 0; i < NUM_CONS; i++){
+		cons[i].bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
 
-	bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
+		bufferevent_setcb(cons[i].bev, readcb, NULL, eventcb,(void *) &cons[i]);
+		bufferevent_enable(cons[i].bev, EV_READ|EV_WRITE); // enable read and write
 
-	bufferevent_setcb(bev, readcb, NULL, eventcb, NULL);
-	bufferevent_enable(bev, EV_READ);
-	bufferevent_disable(bev, EV_WRITE);
-
-	if (bufferevent_socket_connect(bev,
-				(struct sockaddr *)&sin, sizeof(sin)) < 0) {
-		/* Error starting connection */
-		printf("Unable to start connection\n");
-		bufferevent_free(bev);
-		return -1;
+		//bufferevent_disable(bev, EV_WRITE);
+		if(bufferevent_socket_connect_hostname(
+			cons[i].bev, dns_base, AF_INET, cons[i].host, cons[i].port)
+			< 0) {
+			printf("Unable to connect to %s (%s:%d)\n", cons[i].name, cons[i].host, cons[i].port);
+			bufferevent_free(cons[i].bev);
+		}
 	}
+
+
+	//memset(&sin, 0, sizeof(sin));
+	//sin.sin_family = AF_INET;
+	//sin.sin_port = htons(EB_PORT); /* Port 8080 */
 	
+	//if (bufferevent_socket_connect_hostname(
+	//        bev, dns_base, AF_INET, EB_HOST, EB_PORT) < 0) {
+		/* Error connecting */
+	//	printf("Unable to connect to %s:%d\n", EB_HOST, EB_PORT);
+	//	bufferevent_free(bev);
+	//	return -1;
+	//}
+
+	//if (bufferevent_socket_connect(bev,
+	//			(struct sockaddr *)&sin, sizeof(sin)) < 0) {
+		/* Error starting connection */
+	//	printf("Unable to start connection\n");
+	//	bufferevent_free(bev);
+	//	return -1;
+	//}
+	
+	// Create a handler for SIGINT (C-c) so that quitting is nice
 	signal_event = evsignal_new(base, SIGINT, signalcb, (void *)base);
 	if (!signal_event || event_add(signal_event, NULL)<0) {
 		fprintf(stderr, "Could not create/add a signal event!\n");
 		return 1;
 	}
+
+	// Run the event loop
 	event_base_dispatch(base);
+
+	// Cleanup and finish
 	printf("Done.\n");
 	event_free(signal_event);
 	event_base_free(base);
